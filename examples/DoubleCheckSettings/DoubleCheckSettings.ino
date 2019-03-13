sparkfun/SparkFun_LSM6DS3_Arduino_Library

commit 32aca2151b7170c32d848ebe329e07d4a000613d

Make changes to supplied settings visible via pointer - .cpp
User can now provide a pointer to their own SensorSettings structure. If it is supplied it is first filled out to be identical to the desired settings, and then when default cases are selected (because other cases do not match the switch value) the appropriate field is updated in the output SensorSettings. Then the user can double check that SensorSettings matches the settings that they intended. If not then that sub-setting was not a valid choice.

src/SparkFunLSM6DS3.cpp
@@ -388,13 +388,36 @@ LSM6DS3::LSM6DS3( uint8_t busType, uint8_t inputArg ) : LSM6DS3Core( busType, in
 //  "myIMU.settings.accelEnabled = 1;" to configure before calling .begin();
 //
 //****************************************************************************//
-status_t LSM6DS3::begin()
+status_t LSM6DS3::begin(SensorSettings* pActualSettings)
 {
 	//Check the settings structure values to determine how to setup the device
 	uint8_t dataToWrite = 0;  //Temporary variable
 
 	//Begin the inherited core.  This gets the physical wires connected
 	status_t returnError = beginCore();
+	
+	// Copy the values from the user's settings into the output 'pActualSettings'
+	// that will reflect if any default choices had to be made
+	if(pActualSettings != NULL){ 
+		pActualSettings->gyroEnabled = settings.gyroEnabled;
+		pActualSettings->gyroRange = settings.gyroRange;
+		pActualSettings->gyroSampleRate = settings.gyroSampleRate;
+		pActualSettings->gyroBandWidth = settings.gyroBandWidth;
+		pActualSettings->gyroFifoEnabled = settings.gyroFifoEnabled;
+		pActualSettings->gyroFifoDecimation = settings.gyroFifoDecimation;
+		pActualSettings->accelEnabled = settings.accelEnabled;
+		pActualSettings->accelODROff = settings.accelODROff;
+		pActualSettings->accelRange = settings.accelRange;
+		pActualSettings->accelSampleRate = settings.accelSampleRate;
+		pActualSettings->accelBandWidth = settings.accelBandWidth;
+		pActualSettings->accelFifoEnabled = settings.accelFifoEnabled;
+		pActualSettings->accelFifoDecimation = settings.accelFifoDecimation;
+		pActualSettings->tempEnabled = settings.tempEnabled;
+		pActualSettings->commMode = settings.commMode;
+		pActualSettings->fifoThreshold = settings.fifoThreshold;
+		pActualSettings->fifoSampleRate = settings.fifoSampleRate;
+		pActualSettings->fifoModeWord = settings.fifoModeWord;
+	}
 
 	//Setup the accelerometer******************************
 	dataToWrite = 0; //Start Fresh!
@@ -412,6 +435,7 @@ status_t LSM6DS3::begin()
 			dataToWrite |= LSM6DS3_ACC_GYRO_BW_XL_200Hz;
 			break;
 		default:  //set default case to max passthrough
+			if(pActualSettings != NULL){ pActualSettings->accelBandWidth = 400; }
 		case 400:
 			dataToWrite |= LSM6DS3_ACC_GYRO_BW_XL_400Hz;
 			break;
@@ -428,6 +452,7 @@ status_t LSM6DS3::begin()
 			dataToWrite |= LSM6DS3_ACC_GYRO_FS_XL_8g;
 			break;
 		default:  //set default case to 16(max)
+			if(pActualSettings != NULL){ pActualSettings->accelRange = 16; }
 		case 16:
 			dataToWrite |= LSM6DS3_ACC_GYRO_FS_XL_16g;
 			break;
@@ -444,6 +469,7 @@ status_t LSM6DS3::begin()
 			dataToWrite |= LSM6DS3_ACC_GYRO_ODR_XL_52Hz;
 			break;
 		default:  //Set default to 104
+			if(pActualSettings != NULL){ pActualSettings->accelSampleRate = 104; }
 		case 104:
 			dataToWrite |= LSM6DS3_ACC_GYRO_ODR_XL_104Hz;
 			break;
@@ -505,6 +531,7 @@ status_t LSM6DS3::begin()
 			dataToWrite |= LSM6DS3_ACC_GYRO_FS_G_1000dps;
 			break;
 		default:  //Default to full 2000DPS range
+			if(pActualSettings != NULL){ pActualSettings->gyroRange = 2000; }
 		case 2000:
 			dataToWrite |= LSM6DS3_ACC_GYRO_FS_G_2000dps;
 			break;
@@ -521,6 +548,7 @@ status_t LSM6DS3::begin()
 			dataToWrite |= LSM6DS3_ACC_GYRO_ODR_G_52Hz;
 			break;
 		default:  //Set default to 104
+			if(pActualSettings != NULL){ pActualSettings->gyroSampleRate = gyroSampleRate; }
 		case 104:
 			dataToWrite |= LSM6DS3_ACC_GYRO_ODR_G_104Hz;
 			break;

commit 88488f9d08e1a7385e8031ff8d21605094006072

Address issue #5
This completes the handling of issue #5, started with commits 32aca21 and 159af65.

Instead of returning the actual values through the pointer I changed to returning the values that the user wanted through the pointer. Then the actual .settings member of the object is modified to reflect the current settings.

There is now an example to illustrate how to check if the settings have been chnged by the 'begin' method.

examples/DoubleCheckSettings/DoubleCheckSettings.ino
@@ -1,0 +1,99 @@
+/******************************************************************************
+MinimalistExample.ino
+
+Owen Lyke @ SparkFun Electronics
+March 13, 2019
+https://github.com/sparkfun/LSM6DS3_Breakout
+https://github.com/sparkfun/SparkFun_LSM6DS3_Arduino_Library
+
+Description:
+Most basic example of use - except now you can see if your settings got changed
+
+Example using the LSM6DS3 with basic settings.  This sketch collects Gyro and
+Accelerometer data every second, then presents it on the serial monitor.
+
+Resources:
+Uses Wire.h for i2c operation
+Uses SPI.h for SPI operation
+Either can be omitted if not used
+
+Development environment specifics:
+Arduino IDE 1.6.4
+Teensy loader 1.23
+
+Hardware connections:
+Connect I2C SDA line to A4
+Connect I2C SCL line to A5
+Connect GND and 3.3v power to the IMU
+
+This code is released under the [MIT License](http://opensource.org/licenses/MIT).
+
+Please review the LICENSE.md file included with this example. If you have any questions 
+or concerns with licensing, please contact techsupport@sparkfun.com.
+
+Distributed as-is; no warranty is given.
+******************************************************************************/
+
+#include "SparkFunLSM6DS3.h"
+#include "Wire.h"
+#include "SPI.h"
+
+LSM6DS3 myIMU; //Default constructor is I2C, addr 0x6B
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  delay(1000); //relax...
+  Serial.println("Processor came out of reset.\n");
+
+  // Let's choose an unsupported setting...
+  myIMU.settings.accelSampleRate = 404; // Typo, 'meant' to type '104'
+
+  // Make a SensorSettings object to remember what you wanted to set everyhting to
+  SensorSettings settingsIWanted;
+  
+  //Call .begin() to configure the IMU - supplying pointer to the SensorSettings structure
+  myIMU.begin(&settingsIWanted);
+
+  // Compare the sensor settings structure to know if anything was changed
+  compareSettings(settingsIWanted);
+  
+}
+
+
+void loop()
+{
+  //Get all parameters
+  Serial.print("\nAccelerometer:\n");
+  Serial.print(" X = ");
+  Serial.println(myIMU.readFloatAccelX(), 4);
+  Serial.print(" Y = ");
+  Serial.println(myIMU.readFloatAccelY(), 4);
+  Serial.print(" Z = ");
+  Serial.println(myIMU.readFloatAccelZ(), 4);
+
+  Serial.print("\nGyroscope:\n");
+  Serial.print(" X = ");
+  Serial.println(myIMU.readFloatGyroX(), 4);
+  Serial.print(" Y = ");
+  Serial.println(myIMU.readFloatGyroY(), 4);
+  Serial.print(" Z = ");
+  Serial.println(myIMU.readFloatGyroZ(), 4);
+
+  Serial.print("\nThermometer:\n");
+  Serial.print(" Degrees C = ");
+  Serial.println(myIMU.readTempC(), 4);
+  Serial.print(" Degrees F = ");
+  Serial.println(myIMU.readTempF(), 4);
+  
+  delay(1000);
+}
+
+void compareSettings(SensorSettings desiredSettings){
+  if(myIMU.settings.accelBandWidth != desiredSettings.accelBandWidth )    { Serial.println("'accelBandWidth' was changed!"); }
+  if(myIMU.settings.accelRange != desiredSettings.accelRange )            { Serial.println("'accelRange' was changed!"); }
+  if(myIMU.settings.accelSampleRate != desiredSettings.accelSampleRate )  { Serial.println("'accelSampleRate' was changed!"); }
+  if(myIMU.settings.gyroRange != desiredSettings.gyroRange )              { Serial.println("'gyroRange' was changed!"); }
+  if(myIMU.settings.gyroSampleRate != desiredSettings.gyroSampleRate )    { Serial.println("'gyroSampleRate' was changed!"); }
+  // That's all the settings that might get changed in 'begin()'
+}

src/SparkFunLSM6DS3.cpp
@@ -388,7 +388,7 @@ LSM6DS3::LSM6DS3( uint8_t busType, uint8_t inputArg ) : LSM6DS3Core( busType, in
 //  "myIMU.settings.accelEnabled = 1;" to configure before calling .begin();
 //
 //****************************************************************************//
-status_t LSM6DS3::begin(SensorSettings* pActualSettings)
+status_t LSM6DS3::begin(SensorSettings* pSettingsYouWanted)
 {
 	//Check the settings structure values to determine how to setup the device
 	uint8_t dataToWrite = 0;  //Temporary variable
@@ -396,27 +396,27 @@ status_t LSM6DS3::begin(SensorSettings* pActualSettings)
 	//Begin the inherited core.  This gets the physical wires connected
 	status_t returnError = beginCore();
 	
-	// Copy the values from the user's settings into the output 'pActualSettings'
-	// that will reflect if any default choices had to be made
-	if(pActualSettings != NULL){ 
-		pActualSettings->gyroEnabled = settings.gyroEnabled;
-		pActualSettings->gyroRange = settings.gyroRange;
-		pActualSettings->gyroSampleRate = settings.gyroSampleRate;
-		pActualSettings->gyroBandWidth = settings.gyroBandWidth;
-		pActualSettings->gyroFifoEnabled = settings.gyroFifoEnabled;
-		pActualSettings->gyroFifoDecimation = settings.gyroFifoDecimation;
-		pActualSettings->accelEnabled = settings.accelEnabled;
-		pActualSettings->accelODROff = settings.accelODROff;
-		pActualSettings->accelRange = settings.accelRange;
-		pActualSettings->accelSampleRate = settings.accelSampleRate;
-		pActualSettings->accelBandWidth = settings.accelBandWidth;
-		pActualSettings->accelFifoEnabled = settings.accelFifoEnabled;
-		pActualSettings->accelFifoDecimation = settings.accelFifoDecimation;
-		pActualSettings->tempEnabled = settings.tempEnabled;
-		pActualSettings->commMode = settings.commMode;
-		pActualSettings->fifoThreshold = settings.fifoThreshold;
-		pActualSettings->fifoSampleRate = settings.fifoSampleRate;
-		pActualSettings->fifoModeWord = settings.fifoModeWord;
+	// Copy the values from the user's settings into the output 'pSettingsYouWanted'
+	// compare settings with 'pSettingsYouWanted' after 'begin' to see if anything changed
+	if(pSettingsYouWanted != NULL){ 
+		pSettingsYouWanted->gyroEnabled = settings.gyroEnabled;
+		pSettingsYouWanted->gyroRange = settings.gyroRange;
+		pSettingsYouWanted->gyroSampleRate = settings.gyroSampleRate;
+		pSettingsYouWanted->gyroBandWidth = settings.gyroBandWidth;
+		pSettingsYouWanted->gyroFifoEnabled = settings.gyroFifoEnabled;
+		pSettingsYouWanted->gyroFifoDecimation = settings.gyroFifoDecimation;
+		pSettingsYouWanted->accelEnabled = settings.accelEnabled;
+		pSettingsYouWanted->accelODROff = settings.accelODROff;
+		pSettingsYouWanted->accelRange = settings.accelRange;
+		pSettingsYouWanted->accelSampleRate = settings.accelSampleRate;
+		pSettingsYouWanted->accelBandWidth = settings.accelBandWidth;
+		pSettingsYouWanted->accelFifoEnabled = settings.accelFifoEnabled;
+		pSettingsYouWanted->accelFifoDecimation = settings.accelFifoDecimation;
+		pSettingsYouWanted->tempEnabled = settings.tempEnabled;
+		pSettingsYouWanted->commMode = settings.commMode;
+		pSettingsYouWanted->fifoThreshold = settings.fifoThreshold;
+		pSettingsYouWanted->fifoSampleRate = settings.fifoSampleRate;
+		pSettingsYouWanted->fifoModeWord = settings.fifoModeWord;
 	}
 
 	//Setup the accelerometer******************************
@@ -435,7 +435,7 @@ status_t LSM6DS3::begin(SensorSettings* pActualSettings)
 			dataToWrite |= LSM6DS3_ACC_GYRO_BW_XL_200Hz;
 			break;
 		default:  //set default case to max passthrough
-			if(pActualSettings != NULL){ pActualSettings->accelBandWidth = 400; }
+			settings.accelEnabled = 400;
 		case 400:
 			dataToWrite |= LSM6DS3_ACC_GYRO_BW_XL_400Hz;
 			break;
@@ -452,7 +452,7 @@ status_t LSM6DS3::begin(SensorSettings* pActualSettings)
 			dataToWrite |= LSM6DS3_ACC_GYRO_FS_XL_8g;
 			break;
 		default:  //set default case to 16(max)
-			if(pActualSettings != NULL){ pActualSettings->accelRange = 16; }
+			settings.accelRange = 16;
 		case 16:
 			dataToWrite |= LSM6DS3_ACC_GYRO_FS_XL_16g;
 			break;
@@ -469,7 +469,7 @@ status_t LSM6DS3::begin(SensorSettings* pActualSettings)
 			dataToWrite |= LSM6DS3_ACC_GYRO_ODR_XL_52Hz;
 			break;
 		default:  //Set default to 104
-			if(pActualSettings != NULL){ pActualSettings->accelSampleRate = 104; }
+			settings.accelSampleRate = 104;
 		case 104:
 			dataToWrite |= LSM6DS3_ACC_GYRO_ODR_XL_104Hz;
 			break;
@@ -531,7 +531,7 @@ status_t LSM6DS3::begin(SensorSettings* pActualSettings)
 			dataToWrite |= LSM6DS3_ACC_GYRO_FS_G_1000dps;
 			break;
 		default:  //Default to full 2000DPS range
-			if(pActualSettings != NULL){ pActualSettings->gyroRange = 2000; }
+			settings.gyroRange = 2000;
 		case 2000:
 			dataToWrite |= LSM6DS3_ACC_GYRO_FS_G_2000dps;
 			break;
@@ -548,7 +548,7 @@ status_t LSM6DS3::begin(SensorSettings* pActualSettings)
 			dataToWrite |= LSM6DS3_ACC_GYRO_ODR_G_52Hz;
 			break;
 		default:  //Set default to 104
-			if(pActualSettings != NULL){ pActualSettings->gyroSampleRate = gyroSampleRate; }
+			settings.gyroSampleRate = 104;
 		case 104:
 			dataToWrite |= LSM6DS3_ACC_GYRO_ODR_G_104Hz;
 			break;

src/SparkFunLSM6DS3.h
@@ -149,7 +149,7 @@ public:
 	~LSM6DS3() = default;
 	
 	//Call to apply SensorSettings
-	status_t begin(SensorSettings* pActualSettings = NULL);
+	status_t begin(SensorSettings* pSettingsYouWanted = NULL);
 
 	//Returns the raw bits from the sensor cast as 16-bit signed integers
 	int16_t readRawAccelX( void );